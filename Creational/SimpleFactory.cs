70bcbb77f5d23db53bf9012338af5faec118b152
Reza Kazemi <R.Kazemi@Alibaba.ir>
2023-08-14 18:37:48 +0330

    feat: Add Simple factory

---
+++ Creational/SimpleFactory.cs
@@ -0,0 +1,53 @@
+﻿namespace DesignPattern.Creational;
+
+/// <summary>
+/// a factory is an object for creating other objects
+/// – formally a factory is a function or method that returns objects of
+/// a varying prototype or class from some method call
+/// </summary>
+public class SimpleFactory
+{
+    public interface IDesk
+    {
+        int GetHeight();
+        int GetWidth();
+        int GetLength();
+    }
+
+    public class WoodenDesk : IDesk
+    {
+        private int Height { get; set; }
+        private int Width { get; set; }
+        private int Length { get; set; }
+
+        public WoodenDesk(int height, int width, int length)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Length = length;
+        }
+
+        public int GetHeight()
+        {
+            return this.Height;
+        }
+
+        public int GetWidth()
+        {
+            return this.Width;
+        }
+
+        public int GetLength()
+        {
+            return this.Length;
+        }
+    }
+
+    public static class DeskFactory
+    {
+        public static IDesk MakeDesk(int height, int width, int length)
+        {
+            return new WoodenDesk(height, width, length);
+        }
+    }
+}
