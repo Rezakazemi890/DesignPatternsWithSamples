4493ba4a864f4723ec7e084ed837e3c4dd88c78a
Reza Kazemi <R.Kazemi@Alibaba.ir>
2023-08-15 16:38:47 +0330

    feat: Add FactoryMethod Pattern

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using DesignPattern.Creational;
+
+//Singleton
+var instance = Singleton.GetInstance();
+Console.WriteLine($"Singleton : {instance.GetHashCode().ToString()}");
+
+//Simple Factory
+var desk = SimpleFactory.DeskFactory.MakeDesk(80, 30, 40);
+Console.WriteLine(
+    $"SimpleFactory : woodenDesk height: {desk.GetHeight()} - width: {desk.GetWidth()} - length: {desk.GetLength()}");
+
+//Factory Method
+var woodenChairSeller = new FactoryMethod.WoodenChairSeller();
+woodenChairSeller.SellChair();
+
+var metalChairSeller = new FactoryMethod.MetalChairSeller();
+metalChairSeller.SellChair();
+
+//Builder
+var burger = new Builder.BurgerBuilder(15)
+    .AddCheese()
+    .AddLettuce()
+    .AddPepperoni()
+    .Build();
+Console.WriteLine($"{burger.GetDescription()}");
+
+//Abstract
+var woodenWindowFactory = new Abstract.WoodenWindowFactory();
+
+var woodenWindow = woodenWindowFactory.MakeWindow();
+var woodenWindowFittingExpert = woodenWindowFactory.MakeFittingExpert();
+
+woodenWindow.GetDescription();
+woodenWindowFittingExpert.GetDescription();
+
+
+var ironWindowFactory = new Abstract.IronWindowFactory();
+
+var ironWindow = ironWindowFactory.MakeWindow();
+var ironWindowFittingExpert = ironWindowFactory.MakeFittingExpert();
+
+ironWindow.GetDescription();
+ironWindowFittingExpert.GetDescription();
